271649701f8d716bc53f0562183f7681c74349bf
Alexey Savelev <savelichalex93@gmail.com>
2017-09-05 00:25:57 +0300

    Add shared memory file for frame buffer

--- MonitorSimulator/MonitorSimulator/AppDelegate.h
+++ MonitorSimulator/MonitorSimulator/AppDelegate.h
@@ -10,6 +10,8 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (assign) int shm;
+@property (assign) char* addr;
 
 @end
 
--- MonitorSimulator/MonitorSimulator/AppDelegate.m
+++ MonitorSimulator/MonitorSimulator/AppDelegate.m
@@ -2,11 +2,21 @@
 //  AppDelegate.m
 //  MonitorSimulator
 //
-//  Created by Admin on 04.09.17.
+//  Created by savelichalex on 04.09.17.
 //  Copyright © 2017 savelichalex. All rights reserved.
 //
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
 #import "AppDelegate.h"
+
+#define DISPLAY_FRAME_BUFFER "simulator_frame_buffer"
+// 128x128 - dimension, then divide it by off_t size and apply by 8 - one pixel size
+#define DISPLAY_FRAME_BUFFER_SIZE 128 * 128 / sizeof(off_t) * 8
 
 @interface AppDelegate ()
 
@@ -15,12 +25,45 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    int shm;
+    char* addr;
+    
+    if ( (shm = shm_open(DISPLAY_FRAME_BUFFER, O_CREAT|O_RDWR, 0777)) == -1) {
+        int errsv = errno;
+        NSLog(@"Couldn't open frame buffer in shm_open, %i", errsv);
+        
+        return;
+    }
+    
+    if (ftruncate(shm, DISPLAY_FRAME_BUFFER_SIZE) == -1) {
+        int errsv = errno;
+        NSLog(@"Couldn't open frame buffer in ftrancate, %i", errsv);
+        
+        return;
+    }
+    
+    self.shm = shm;
+    
+    addr = mmap(0, DISPLAY_FRAME_BUFFER_SIZE, PROT_READ, MAP_SHARED, shm, 0);
+    
+    if (addr == (char*)-1) {
+        int errsv = errno;
+        NSLog(@"Couldn't open frame buffer in mmap, %i", errsv);
+        
+        return;
+    }
+    
+    NSLog(@"Openning success");
+    
+    self.addr = addr;
+    
+    
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    munmap(self.addr, DISPLAY_FRAME_BUFFER_SIZE);
+    shm_unlink(DISPLAY_FRAME_BUFFER);
 }
 
 
