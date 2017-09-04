1579db7bcfb41210c8684712c6ff68513f3909e8
Alexey Savelev <savelichalex93@gmail.com>
2017-09-04 13:24:56 +0300

    Initil commit

---
+++ MonitorSimulator/MonitorSimulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MonitorSimulator
+//
+//  Created by Admin on 04.09.17.
+//  Copyright © 2017 savelichalex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MonitorSimulator/MonitorSimulator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MonitorSimulator
+//
+//  Created by Admin on 04.09.17.
+//  Copyright © 2017 savelichalex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
