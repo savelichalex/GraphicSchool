69ba0154f398927b8c56fa74dfa12d49af8cf5b3
Alexey Savelev <savelichalex93@gmail.com>
2017-09-13 23:46:14 +0300

    Add DDA line algoritm

---
+++ Lines/line.ml
@@ -0,0 +1,29 @@
+type point = { x: int; y: int }
+
+let get_steps_count dx dy =
+  if abs dx > abs dy then abs dx
+  else abs dy
+
+let get_pixel_point a b =
+  float_of_int a +. b |> floor |> truncate
+
+let rec draw_line_DDA x y x_inc y_inc steps count =
+  if count == steps then ()
+  else begin
+    Pixellib.set_pixel_exp x y 0;
+    draw_line_DDA
+      (get_pixel_point x x_inc)
+      (get_pixel_point y y_inc)
+      x_inc y_inc steps (count + 1)
+  end
+
+let line_DDA ~start:start ~close:close =
+  let dx = close.x - start.x
+  and dy = close.y - start.y in
+    let steps = get_steps_count dx dy in
+      let x_increment = (float_of_int dx) /. (float_of_int steps)
+      and y_increment = (float_of_int dy) /. (float_of_int steps) in
+        draw_line_DDA start.x start.y x_increment y_increment steps 0
+
+let () =
+  line_DDA {x=0; y=0} {x=5;y=5}
--- PixelLib/pixellib.ml
+++ PixelLib/pixellib.ml
@@ -1,23 +1,4 @@
 (* open Printf *)
 
-external openDisplayBufferExp: unit -> unit = "open_display_buffer"
-external setPixelExp: int -> int -> int -> unit = "set_pixel"
-
-
-let rec drawLine line =
-  match line with
-  | [] -> ()
-  | first :: rest ->
-      let (x, y) = first in
-        Printf.printf "%i, %i" x y;
-        setPixelExp x y 0;
-      drawLine rest
-  ;;
-
-
-let () =
-  try
-    openDisplayBufferExp ();
-    drawLine [(5,5);(6,6);(7,7)]
-  with
-  | Failure (a) -> print_string a
+external open_display_buffer_exp: unit -> unit = "open_display_buffer"
+external set_pixel_exp: int -> int -> int -> unit = "set_pixel"
